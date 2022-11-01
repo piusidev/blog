03a4e5002af39d98ef59d7d843d02baad8a132e4
Leonardo Piusi <leopidev@gmail.com>
2022-11-01 01:26:31 -0300

    chore: axios config

---
+++ src/services/api.ts
@@ -0,0 +1,5 @@
+import { axiosClient } from './axios-config';
+
+const api = axiosClient();
+
+export default api;
---
+++ src/services/axios-config.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosInstance } from 'axios';
+
+export const axiosClient = (): AxiosInstance => {
+  const baseURL = process.env.BONFIRE_SERVER_ENDPOINT;
+  const api = axios.create({
+    baseURL,
+    headers: {
+      'access-control-allow-origin': '*',
+      'access-control-allow-methods': '*',
+      'access-control-allow-headers': '*',
+      'Content-Type': 'application/json'
+    }
+  });
+
+  api.interceptors.response.use();
+
+  return api;
+};
